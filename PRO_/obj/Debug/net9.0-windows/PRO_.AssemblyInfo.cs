0daca021b70d3ba34c2ff42eadb5cae8f0cb7625
maksc <s28406@pjwstk.edu.pl>
2025-08-22 19:59:24 +0200

    Added confirmation window

--- PRO_/obj/Debug/net9.0-windows/PRO_.AssemblyInfo.cs
+++ PRO_/obj/Debug/net9.0-windows/PRO_.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRO_")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba614e3ef97874cfa1cdc7a8dfd524737d89d4e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+396b6c19626359e492f5f52def33bf734c01d611")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRO_")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRO_")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
